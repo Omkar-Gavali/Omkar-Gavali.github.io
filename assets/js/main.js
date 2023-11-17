2ac2986fc4e44af02eb18071035c6ffb830b700d
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2023-11-17 15:13:38 +0100

    cc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*=============== REMOVE MENU MOBILE ===============*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
