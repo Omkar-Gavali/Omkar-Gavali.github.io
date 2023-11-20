3d45a898c4f82d3b4429e129334d36c67895bced
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2023-11-20 17:50:28 +0100

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*=============== REMOVE MENU MOBILE ===============*/
+
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
@@ -16,7 +16,21 @@ if(navClose){
     })
 }
 
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+*/
 /*=============== ADD BLUR TO HEADER ===============*/
 
 
