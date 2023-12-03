4e436b1c8a72b94ea3f78f438b0ed5c3474abd91
omkar <Omkar.Gavali@Student.HTW-Berlin.de>
2023-12-03 17:30:04 +0100

    nnew

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,12 @@ function scrollHeader(){
 }
 */
 /*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 
 /*=============== EMAIL JS ===============*/
